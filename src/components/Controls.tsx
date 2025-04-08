cc31d41ea845f520e0bbc3a035c8b4474f1e993e
Jon Jaques <jaquers@gmail.com>
2025-04-07 20:52:32 -0500

    Update deps, remove unused deps

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import { Button, Collapse } from "react-bootstrap";
 import ControlsForm from "./ControlsForm";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Controls() {
   const [open, setOpen] = useState(false);
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,5 +1,4 @@
 import { useId } from "react";
-import { Form } from "react-bootstrap";
 import {
   useController,
   type UseControllerProps,
