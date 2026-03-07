29f6e5e6c5ddc6e8e1a81099b7f152bb2e386909
Jon Jaques <jaquers@gmail.com>
2026-03-07 13:56:07 -0600

    Update page manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mandelbro.jonjaques.com",
   vite: {
     plugins: [tailwindcss()],
   },
